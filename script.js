63859c95753cec2d7b5e14dd0dffd05321c820f1
pico0513 <6myjk9yycv@privaterelay.appleid.com>
2026-08-28 08:01:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function showMessage() {
-    alert("ライブ登録画面は準備中です！");
+function showForm() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("form").style.display = "block";
+}
+
+function showHome() {
+    document.getElementById("form").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
+
+function saveLive() {
+    alert("ライブを保存しました！");
 }
